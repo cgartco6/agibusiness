6394b8b6816c835cb189c1178e84aaeaaa0f7994
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2025-08-13 01:11:08 +0200

    Delete backendapp directory

--- backendapp/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django_cryptography.fields import encrypt
-
-class ClientProject(models.Model):
-    project_id = encrypt(models.CharField(max_length=64))
-    assets = encrypt(models.JSONField()))
-    created_at = models.DateTimeField(auto_now_add=True)
